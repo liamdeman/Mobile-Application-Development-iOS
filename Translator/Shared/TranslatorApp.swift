feda19fd076e624dcc175ab413010bc5bd0f9827
Liam Deman <liam.deman@student.hogent.be>
2022-08-19 16:10:02 +0200

    Add project

---
+++ Translator/Shared/TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatorApp.swift
+//  Shared
+//
+//  Created by Liam Deman on 19/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
