ecc61a01d5bc538de9a4c44848ce2c63f8843db8
Liam Deman <liam.deman@student.hogent.be>
2022-08-31 18:56:47 +0200

    Add translation functionality

---
+++ Translator/Models/Translation.swift
@@ -0,0 +1,5 @@
+
+import Foundation
+struct Translation : Codable {
+    var translatedText: String
+}
--- Translator/ViewModels/TranslationViewModel.swift
+++ Translator/ViewModels/TranslationViewModel.swift
@@ -4,9 +4,62 @@ extension TranslationView{
     @MainActor class TranslationViewModel: ObservableObject {
         @Published var originalText: String = "";
         @Published var translatedText: String = "Click 'Translate!' to start translation";
-        
+        private let apiUrl = "https://libretranslate.de/"
+
         func translate() {
-            translatedText = originalText + "(translated)";
+            if (originalText.isEmpty){
+                translatedText = "No text entered"
+            }
+            else {
+                fetchTranslation()
+            }
         }
+        
+        func fetchTranslation() -> Void {
+            var httpRequest = URLRequest(url: URL(string: "\(apiUrl)translate")!)
+            httpRequest.httpMethod = "POST"
+            
+            let body: [String: String] =
+            [
+            "q": originalText,
+            "source": self.getLanguage(type: LanguageSelectionType.source),
+            "target": self.getLanguage(type: LanguageSelectionType.destination)
+            ]
+            httpRequest.httpBody = self.stringifyDictonary(dict: body).data(using: .utf8)
+            
+            URLSession.shared.dataTask(with: httpRequest) { data, response,error in
+                if (error == nil && data != nil){
+                    let responseData = try! JSONDecoder().decode(Translation.self, from: data!)
+                    self.translatedText = responseData.translatedText
+                }
+                else{
+                    self.translatedText = "Server Error"
+                }
+                
+            }.resume()
+        }
+        
+        
+        //method copied from stackoverflow.com
+        func stringifyDictonary(dict:[String:String]) -> String
+            {
+                var entries = [String]()
+                for(key, value) in dict
+                {
+                    entries.append(key + "=\(value)")
+                }
+                return entries.map { String($0) }.joined(separator: "&")
+            }
+        
+        func getLanguage(type: LanguageSelectionType) -> String
+        {
+            if let data = UserDefaults.standard.data(forKey: type.rawValue) {
+                    if let decoded = try? JSONDecoder().decode(Language.self, from: data) {
+                        return decoded.code
+                    }
+                }
+            return type == LanguageSelectionType.source ? "en" : "es"
+        }
+
     }
 }
